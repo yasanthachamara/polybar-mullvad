7f29f3845d04f23b5354e7ea09f75bd359cf24b7
yasanthachamara <yasantha666@gmail.com>
2020-04-26 15:04:23 +0530

    Modified the code

--- vpn.sh
+++ vpn.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
-vpn_connected=$(mullvad status | grep 'Connected to WireGuard' | awk '{print $3}')
+vpn_connected=$(mullvad status | grep 'Connected to' | awk '{print $3}')
 
 if [  "$vpn_connected" == "Connected" ]; then
-		echo "$(mullvad status | grep 'Connected to WireGuard' | awk '{print$7}' | cut -d ':' -f 1)"
+		echo "$(mullvad status | awk '{print$7}' | cut -d ':' -f 1)"
 else
 		echo "Not Connected"
 fi
